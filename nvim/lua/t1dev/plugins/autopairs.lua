a0e61b0cc9e34b3391633303c8de9ad675eed6eb
0xb7a7dd61 <t1dev@icloud.com>
2023-11-15 14:19:53 -0700

    fix: only do block commenting for solidity

--- nvim/lua/t1dev/plugins/autopairs.lua
+++ nvim/lua/t1dev/plugins/autopairs.lua
@@ -11,7 +11,7 @@ return {
             npairs.setup(opts)
 
             npairs.add_rules({
-                Rule("/*", " */"),
+                Rule("/*", " */", { "solidity" }),
             })
         end,
     },
