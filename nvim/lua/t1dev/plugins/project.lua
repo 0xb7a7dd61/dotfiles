33e8ca2e66364aaec3004900b4508a10245eec2f
0xb7a7dd61 <t1dev@icloud.com>
2023-12-11 12:17:19 -0700

    feat: add folder picker inside grep

--- nvim/lua/t1dev/plugins/project.lua
+++ nvim/lua/t1dev/plugins/project.lua
@@ -1,5 +1,9 @@
 return {
     {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    },
+    {
         "nvim-telescope/telescope.nvim",
         opts = function(_, opts)
             opts.extensions = {
@@ -12,6 +16,47 @@ return {
                     },
                 },
             }
+
+            local ts_select_dir_for_grep = function(prompt_bufnr)
+                local action_state = require("telescope.actions.state")
+                local fb = require("telescope").extensions.file_browser
+                local live_grep = require("telescope.builtin").live_grep
+                local current_line = action_state.get_current_line()
+
+                fb.file_browser({
+                    files = false,
+                    depth = false,
+                    attach_mappings = function(prompt_bufnr2)
+                        require("telescope.actions").select_default:replace(function()
+                            local entry_path = action_state.get_selected_entry().Path
+                            local dir = entry_path:is_dir() and entry_path or entry_path:parent()
+                            local relative = dir:make_relative(vim.fn.getcwd())
+                            local absolute = dir:absolute()
+
+                            live_grep({
+                                results_title = relative .. "/",
+                                cwd = absolute,
+                                default_text = current_line,
+                            })
+                        end)
+
+                        return true
+                    end,
+                })
+            end
+
+            opts.pickers = vim.tbl_extend("force", opts.pickers or {}, {
+                live_grep = {
+                    mappings = {
+                        i = {
+                            ["<C-l>"] = ts_select_dir_for_grep,
+                        },
+                        n = {
+                            ["<C-l>"] = ts_select_dir_for_grep,
+                        },
+                    },
+                },
+            })
         end,
     },
     -- project management. telescope-project allows us to hook the project selection to manage sessions
