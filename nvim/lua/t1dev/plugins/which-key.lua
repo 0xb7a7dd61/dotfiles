cbb266a65ff8976751592dafcaebc87a451958a2
0xb7a7dd61 <t1dev@icloud.com>
2024-02-29 14:13:43 -0700

    chore: rename +prefix to +toggle for <leader>t

--- nvim/lua/t1dev/plugins/which-key.lua
+++ nvim/lua/t1dev/plugins/which-key.lua
@@ -5,6 +5,7 @@ return {
             defaults = {
                 -- add helpful names to the command tree. without this they show as "+prefix"
                 ["<leader>r"] = { name = "+regex" },
+                ["<leader>t"] = { name = "+toggle" },
             },
         },
     },
