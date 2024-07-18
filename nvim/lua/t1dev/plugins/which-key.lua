04247116014acfc79c79600cff9299038a06dfbf
0xb7a7dd61 <t1dev@icloud.com>
2024-07-18 10:11:09 -0700

    fix: updated dependencies; which-key changed spec format

--- nvim/lua/t1dev/plugins/which-key.lua
+++ nvim/lua/t1dev/plugins/which-key.lua
@@ -1,12 +1,13 @@
 return {
-    {
-        "folke/which-key.nvim",
-        opts = {
-            defaults = {
-                -- add helpful names to the command tree. without this they show as "+prefix"
-                ["<leader>r"] = { name = "+regex" },
-                ["<leader>t"] = { name = "+toggle" },
-            },
-        },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {},
+      spec = {
+        -- add helpful names to the command tree. without this they show as "+prefix"
+        { "<leader>r", name = "+regex" },
+        { "<leader>t", name = "+toggle" },
+      },
     },
+  },
 }
